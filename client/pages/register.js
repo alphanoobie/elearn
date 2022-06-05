350169264de44d75a5226ddfe659db6bd7f34bf7
alphanoobie <hussaingadi.hg@gmail.com>
2022-06-05 16:46:07 +0530

    notifs and loading spinner

--- client/pages/register.js
+++ client/pages/register.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react'
 import { Card, Form, Input, Button } from 'antd'
 import styles from '../public/css/register.module.css'
 import axios from 'axios'
+import { toast } from 'react-toastify'
+import { SyncOutlined } from '@ant-design/icons'
 
 export default function Register() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async () => {
-        console.table({name, email, password});
-        const {data} = await axios.post(`http://localhost:8000/api/register`, {name,email,password})
-        console.log('REGISTER RESPONSE', data)
+        try {
+            setLoading(true)
+            //console.table({name, email, password});
+            const { data } = await axios.post(`http://localhost:8000/api/register`, { name, email, password })
+            // console.log('REGISTER RESPONSE', data)
+            toast('Registeration successful, please login')
+            setLoading(false)
+        } catch (err) {
+            toast(err.response.data)
+            setLoading(false)
+        }
     }
 
     return (
@@ -24,7 +35,7 @@ export default function Register() {
                     labelCol={{ span: 6 }}
                     labelWrap
                     labelAlign='left'
-                    onFinish = {handleSubmit}
+                    onFinish={handleSubmit}
                 >
                     <Form.Item
                         label="Name"
@@ -62,8 +73,8 @@ export default function Register() {
                     </Form.Item>
 
                     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit">
-                            Submit
+                        <Button style={{width:80}} type="primary" htmlType="submit" disabled={!name || !email || !password || loading}>
+                            {loading? <SyncOutlined spin/> : "Submit"}
                         </Button>
                     </Form.Item>
                 </Form>
