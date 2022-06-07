4f713079039fcbcf5d0a2685928f9b1af583ea66
deathmukh <91791452+deathmukh@users.noreply.github.com>
2022-06-07 21:38:45 +0530

    AWS, forgot pwd

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,10 +4,11 @@ import { requireSignIn } from "../middlewares";
 const router = express.Router();
 
 //controller
-import { register, login, logout, currentUser } from "../controllers/auth";
+import { register, login, logout, currentUser,sendTestEmail } from "../controllers/auth";
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/current-user", requireSignIn, currentUser);
+router.get("/send-email", sendTestEmail)
 module.exports = router;
