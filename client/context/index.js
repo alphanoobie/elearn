04bc994fc55819af2ad3eca980b33c378525c614
alphanoobie <hussaingadi.hg@gmail.com>
2022-06-07 07:06:08 +0530

    axios interceptor

--- client/context/index.js
+++ client/context/index.js
@@ -1,4 +1,6 @@
 import { useReducer, createContext, useEffect } from "react";
+import axios from 'axios'
+import useRouter from 'next/router'
 
 //initial state
 const initialState = {
@@ -26,12 +28,40 @@ const rootReducer = (state, action) => {
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(rootReducer, initialState);
 
+  const router = useRouter()
+  
   useEffect(() => {
     dispatch({
       type: "LOGIN",
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
   }, []);
+
+  axios.interceptors.response.use(
+    function(response){
+      return response
+    },
+    function(error){
+      let res = error.response
+      if (res.status===401 && res.config && !res.config.__isRetryRequest){
+        return new Promise((resolve,reject)=>{
+          axios.get('/api/logout')
+          .then((data)=>{
+            console.log('401 error >logout')
+            dispatch({type:'LOGOUT'})
+            window.localStorage.removeItem('user')
+            router.push('/login')
+          })
+          .catch(err=>{
+            console.log("AXIOS INTERCEPTOR ERROR", err)
+            reject(err)
+          })
+        })
+      }
+      return Promise.reject(error);
+    }
+  )
+
   return (
     <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
   );
