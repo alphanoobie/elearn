1102b92dbfceca919773b01f3806aa20575e8f8e
alphanoobie <hussaingadi.hg@gmail.com>
2022-06-11 15:41:05 +0530

    create course from 1st step

--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
+import { Button, Select } from 'antd'
+import { SaveOutlined } from "@ant-design/icons";
+
+const { Option } = Select
 
 export default function CourseCreate() {
   const [values, setValues] = useState({
@@ -39,7 +43,7 @@ export default function CourseCreate() {
           />
         </div>
 
-        <div className="form-group">
+        <div className="form-group pt-3">
           <textarea
             name="description"
             cols="7"
@@ -47,7 +51,50 @@ export default function CourseCreate() {
             value={values.description}
             className="form-control"
             onChange={handleChange}
+            placeholder="Description"
           />
+        </div>
+
+        <div className="form-row pt-3">
+          <div className="col">
+            <div className="form-group">
+              <Select
+                size='large'
+                style={{ width: '100%' }}
+                value={values.paid}
+                onChange={v => setValues({ ...values, paid: !values.paid })}
+              >
+                <Option value={true}>Paid</Option>
+                <Option value={false}>Free</Option>
+              </Select>
+            </div>
+          </div>
+        </div>
+
+        <div className="form-row pt-3">
+          <div className="col">
+            <div className="form-group">
+              <label className="btn btn-outline-secondary btn-block text-left">
+                {values.loading ? 'Uploading' : 'Image Upload'}
+                <input type="file" name="image" onChnage={handleImage} accept="image/*" hidden />
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="row pt-3" >
+          <div className="col">
+            <Button onClick={handleSubmit} 
+            disabled={values.loading || values.uploading}
+            className="btn btn-primary"
+            loading={values.loading}
+            type='primary'
+            size="large"
+            shape='round'
+            >
+              {values.loading ? 'Saving...' : 'Save & Continue'}
+            </Button>
+          </div>
         </div>
       </form>
     );
