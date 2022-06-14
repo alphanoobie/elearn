c05b270b5e546f7f5ac10ca8d01ad73b5d9be591
ishani <91791452+deathmukh@users.noreply.github.com>
2022-06-14 13:14:21 +0530

    get courses from backend

--- client/pages/instructor/index.js
+++ client/pages/instructor/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import InstructorRoute from "../../components/routes/InstructorRoute";
 
 export default function InstructorIndex() {
+
+  const [courses, setcourses] = useState([])
+
+  useEffect(() => {
+    loadCourses()
+  }, [])
+
+  const loadCourses = async () => {
+    const { data } = await axios.get('api/instructor-courses')
+    setcourses(data)
+  }
+
   return (
     <InstructorRoute>
       <h1 className="jumbotron text-center">Instructor Dashboard</h1>
+      <pre>{JSON.stringify(courses, null, 4)}</pre>
     </InstructorRoute>
   );
 }
