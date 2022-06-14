34cf42ca5a3a691b90540ed75a21fc2c76f84f26
ishani <91791452+deathmukh@users.noreply.github.com>
2022-06-14 13:15:49 +0530

    backend course route

--- server/routes/instructor.js
+++ server/routes/instructor.js
@@ -8,10 +8,13 @@ import {
   makeInstructor,
   getAccountStatus,
   currentInstructor,
+  instructorCourses
 } from "../controllers/instructor";
 
 router.post("/make-instructor", requireSignIn, makeInstructor);
 router.post("/get-account-status", requireSignIn, getAccountStatus);
 router.get("/current-instructor", requireSignIn, currentInstructor);
+
+router.get('/instructor-courses', requireSignIn, instructorCourses)
 
 module.exports = router;
