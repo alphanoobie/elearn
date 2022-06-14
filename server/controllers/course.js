2dca8d441acf66b644604589cfd457d7548ead00
ishani <91791452+deathmukh@users.noreply.github.com>
2022-06-14 12:40:12 +0530

    course create function

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -1,5 +1,7 @@
 import AWS from 'aws-sdk'
 import { nanoid } from 'nanoid'
+import Course from '../models/course'
+import slugify from 'slugify'
 
 const awsConfig = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -14,7 +16,7 @@ const S3 = new AWS.S3(awsConfig)
 export const uploadImage = async (req, res) => {
     // console.log(req.body)
     try {
-        const {image} = req.body
+        const { image } = req.body
         if (!image) return res.status(400).send('No Image')
 
         //prepare image
@@ -28,16 +30,16 @@ export const uploadImage = async (req, res) => {
             Key: `${nanoid()}.${type}`,
             Body: base64Data,
             ACL: 'public-read',
-            ContentEncoding : 'base64',
+            ContentEncoding: 'base64',
             ContentType: `image/${type}`
         }
 
         //upload to s3
-        S3.upload(params, (err,data)=>{
-            if (err){
+        S3.upload(params, (err, data) => {
+            if (err) {
                 console.log(err)
                 return res.sendStatus(400)
-            } 
+            }
             console.log(data)
             res.send(data)
         })
@@ -47,27 +49,46 @@ export const uploadImage = async (req, res) => {
     }
 }
 
-export const removeImage = async(req, res) => {
-    try{
-        const {image} = req.body
+export const removeImage = async (req, res) => {
+    try {
+        const { image } = req.body
         const params = {
             Bucket: image.Bucket,
-            Key:image.Key,
+            Key: image.Key,
         }
 
         //send remove req
-        S3.deleteObject(params, (err, data)=> {
+        S3.deleteObject(params, (err, data) => {
             if (err) {
                 console.log(err)
                 res.sendStatus(400)
             }
-            res.send({ok:true})
+            res.send({ ok: true })
         })
     } catch (err) {
         console.log(err)
     }
 }
 
-export const create = async(req, res) => {
-    console.log("Create Course")
+export const create = async (req, res) => {
+    // console.log("Create Course", req.body)
+    try {
+        const alreadyExist = await Course.findOne({
+            slug: slugify(req.body.name.toLowerCase())
+        })
+        if (alreadyExist) {
+            return res.status(400).send('Title is taken')
+        }
+
+        const course = await new Course({
+            slug: slugify(req.body.name),
+            instructor: req.auth._id,
+            ...req.body,
+        }).save()
+        res.json(course)
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send('Course create failed')
+    }
 }
