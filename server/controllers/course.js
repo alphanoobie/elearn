ed8d612a09ff0dc6c9c25b30716500604993dae1
ishani <91791452+deathmukh@users.noreply.github.com>
2022-07-15 20:28:01 +0530

    remove video backend

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -133,3 +133,26 @@ export const uploadVideo = async (req, res) => {
     console.log(error);
   }
 };
+
+
+export const removeVideo = async (req, res) => {
+  try {
+    const { Bucket, Key } = req.body;
+    console.log('===========>', req.body)
+    const params = {
+      Bucket,
+      Key,
+    };
+
+    S3.deleteObject(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.sendStatus(400);
+      }
+      console.log(data);
+      res.send({ ok: true });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
