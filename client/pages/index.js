8c03f1f28038f601a7b95a076692a3b7e3aa9abc
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-24 02:15:42 +0530

    course card component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CourseCard from "../components/cards/CourseCard";
+
 const Index = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const { data } = await axios.get("/api/courses");
+      setCourses(data);
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <>
       <h1 className="jumbotron text-center bg-primary border-r-6">
         E-Learn App
       </h1>
-      <p>from nextjs...</p>
+
+      <div className="container-fluid">
+        <div className="row">
+          {courses.map((course) => (
+            <div key={course._id} className="col-md-4">
+              {/* <pr>{JSON.stringify(course, null, 4)}</pr> */}
+              <CourseCard course={course} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
