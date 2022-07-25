3606d97d4f3261991bfea24b2ca81c9a9e578f0f
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-25 21:04:13 +0530

    single course list component basic

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import SingleCourseJumbotron from "../../components/cards/SingleCourseJumbotron";
 import PreviewModal from "../../components/modal/PreviewModal";
+import SingleCourseLessons from "../../components/cards/SingleCourseLessons";
 
 const SingleCourse = ({ course }) => {
   const [showModal, setShowModal] = useState(false);
@@ -21,7 +22,20 @@ const SingleCourse = ({ course }) => {
         setPreview={setPreview}
       />
 
-      <PreviewModal showModal={showModal} setShowModal={setShowModal} preview={preview}/>
+      <PreviewModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        preview={preview}
+      />
+
+      {course.lessons && (
+        <SingleCourseLessons
+          lessons={course.lessons}
+          setPreview={setPreview}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      )}
     </>
   );
 };
