d34aaac8d83bf7b1d6e8ea42d44e72628f98b122
ishani <91791452+deathmukh@users.noreply.github.com>
2022-07-25 13:51:43 +0530

    added currency formatter

--- client/components/cards/CourseCard.js
+++ client/components/cards/CourseCard.js
@@ -1,5 +1,6 @@
 import { Card, Badge } from "antd";
 import Link from "next/link";
+import { currencyFormatter } from "../../utils/helpers";
 
 const { Meta } = Card;
 
@@ -15,14 +16,18 @@ const CourseCard = ({ course }) => {
               src={image && image.Location}
               alt={name}
               style={{ height: "200px", objectFit: "cover" }}
-                className="p-1"
+              className="p-1"
             />
           }
         >
           <h2 className="font-weight-bold">{name}</h2>
           <p>By {instructor.name}</p>
-          <Badge count={category} style={{backgroundColor:'#03a9f4'}} className="pb-2 mr-2"/>
-          <h4 className="pt-2">{paid? price : "Free"}</h4>
+          <Badge
+            count={category}
+            style={{ backgroundColor: "#03a9f4" }}
+            className="pb-2 mr-2"
+          />
+          <h4 className="pt-2">{paid ? currencyFormatter({amount:price, currency: "inr"}) : "Free"}</h4>
         </Card>
       </a>
     </Link>
