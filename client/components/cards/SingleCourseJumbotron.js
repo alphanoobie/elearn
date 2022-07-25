788938f0d50dd75da27e1fcdda16a33c04190976
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-25 20:42:24 +0530

    single course wrapper component

---
+++ client/components/cards/SingleCourseJumbotron.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { currencyFormatter } from "../../utils/helpers";
+import { Badge, Modal } from "antd";
+import ReactPlayer from "react-player";
+
+export default function SingleCourseJumbotron({
+  course,
+  showModal,
+  setShowModal,
+  preview,
+  setPreview,
+}) {
+  const {
+    name,
+    description,
+    instructor,
+    updatedAt,
+    lessons,
+    image,
+    price,
+    paid,
+    category,
+  } = course;
+
+  return (
+    <div className="jumbotron bg-primary square">
+      <div className="row">
+        <div className="col-md-8">
+          <h1 className="text-light font-weight-bold">{name}</h1>
+          <p className="lead">
+            {description && description.substring(0, 160)}...
+          </p>
+          <Badge
+            count={category}
+            style={{ backgroundColor: "#03a9f4" }}
+            className="pb-4 mr-2"
+          />
+          <p> Created by {instructor.name}</p>
+          <p> Last updated {new Date(updatedAt).toLocaleDateString()}</p>
+          <h4 className="text-light">
+            {paid
+              ? currencyFormatter({ amount: price, currency: "inr" })
+              : "Free"}
+          </h4>
+        </div>
+        <div className="col-md-4">
+          {lessons[0].video && lessons[0].video.Location ? (
+            <div
+              onClick={() => {
+                setPreview(lessons[0].video.Location);
+                setShowModal(!showModal);
+              }}
+            >
+              <ReactPlayer
+                className="react-player-div"
+                url={lessons[0].video.Location}
+                light={image && image.Location}
+                width="100%"
+                height="225px"
+              />
+            </div>
+          ) : (
+            <>
+              <img
+                src={image && image.Location}
+                alt={name}
+                className="img img-fluid"
+              />
+            </>
+          )}
+          <p>show enroll button</p>
+        </div>
+      </div>
+    </div>
+  );
+}
