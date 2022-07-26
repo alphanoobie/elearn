2a847f5e6fbc827e6d30fdef75857c33e0a2c11f
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-26 16:00:32 +0530

    show course videos and sidebar

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,8 +1,15 @@
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, createElement } from "react";
 import axios from "axios";
 import StudentRoute from "../../../components/routes/StudentRoute";
 import { Button, Menu, Avatar } from "antd";
+import ReactPlayer from "react-player";
+import ReactMarkdown from "react-markdown";
+import {
+  PlayCircleOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from "@ant-design/icons";
 
 const { Item } = Menu;
 
@@ -27,6 +34,13 @@ const SingleCourse = () => {
     <StudentRoute>
       <div className="row">
         <div style={{ maxWidth: 320 }}>
+          <Button
+            className="text-primary mt-1 btn-block mb-2"
+            onClick={() => setCollapsed(!collapsed)}
+          >
+            {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}{" "}
+            {!collapsed && "Lessons"}
+          </Button>
           <Menu
             defaultSelectedKeys={[clicked]}
             inlineCollapsed={collapsed}
@@ -46,9 +60,34 @@ const SingleCourse = () => {
 
         <div className="col">
           {clicked !== -1 ? (
-            <>{JSON.stringify(course.lessons[clicked])}</>
+            <>
+              {course.lessons[clicked].video &&
+                course.lessons[clicked].video.Location && (
+                  <>
+                    <div className="wrapper">
+                      <ReactPlayer
+                        className="player"
+                        playing={false}
+                        controls
+                        url={course.lessons[clicked].video.Location}
+                        width="100%"
+                        height="100%"
+                      />
+                    </div>
+                  </>
+                )}
+              <ReactMarkdown
+                children={course.lessons[clicked].content}
+                className="single-post"
+              />
+            </>
           ) : (
-            <>Click on the lesson to start learning</>
+            <div className="d-flex justify-content-center p-5">
+              <div className="text-center p-5">
+                <PlayCircleOutlined className="text-primary display-1 p-5" />
+                <p className="lead">Click on lessons to start learning</p>
+              </div>
+            </div>
           )}
         </div>
       </div>
