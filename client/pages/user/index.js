ab8f4a94cef56c86e9126c2fca6e46744b174ca8
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-26 13:58:27 +0530

    user dashboard frontend

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../context";
 import UserRoute from "../../components/routes/UserRoute";
+import axios from "axios";
 
 export default function UserIndex() {
   const {
     state: { user },
   } = useContext(Context);
 
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
+  const loadCourses = async () => {
+    const { data } = await axios.get("/api/user-courses");
+    setCourses(data);
+  };
   return (
     <UserRoute>
       <h1 className="jumbotron text-center">User Dashboard</h1>
+      <pre>{JSON.stringify(courses, null, 4)}</pre>
     </UserRoute>
   );
 }
