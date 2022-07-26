2bf636e8efca0c3741c2db6540389c7a39cd3daa
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-26 11:09:22 +0530

    enrollment button frontend

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -3,7 +3,8 @@ import { nanoid } from "nanoid";
 import Course from "../models/course";
 import slugify from "slugify";
 import { readFileSync } from "fs";
-import user from "../models/user";
+
+import User from "../models/user";
 
 const awsConfig = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -311,11 +312,12 @@ export const courses = async (req, res) => {
 
 export const checkEnrollment = async (req, res) => {
   const { courseId } = req.params;
-  const user = await user.findById(req.auth._id).exec();
+  const user = await User.findById(req.auth._id).exec();
 
   //check if course is found user courses
   let ids = [];
-  for (let i = 0; i < user.courses.length; i++) {
+  let length = user.courses && user.courses.length
+  for (let i = 0; i < length; i++) {
     ids, push(user.courses[i].toString());
   }
   res.json({
