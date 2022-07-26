2708884eb115b1f90fa4ef54c8209d9296e6f7a9
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-26 15:36:47 +0530

    lesson sidebar

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -2,11 +2,15 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import StudentRoute from "../../../components/routes/StudentRoute";
+import { Button, Menu, Avatar } from "antd";
+
+const { Item } = Menu;
 
 const SingleCourse = () => {
   const router = useRouter();
   const { slug } = router.query;
-
+  const [clicked, setClicked] = useState(-1);
+  const [collapsed, setCollapsed] = useState(false);
   const [loading, setLoading] = useState(false);
   const [course, setCourse] = useState({ lessons: [] });
 
@@ -21,7 +25,33 @@ const SingleCourse = () => {
 
   return (
     <StudentRoute>
-      <h1>{JSON.stringify(course, null, 4)}</h1>
+      <div className="row">
+        <div style={{ maxWidth: 320 }}>
+          <Menu
+            defaultSelectedKeys={[clicked]}
+            inlineCollapsed={collapsed}
+            style={{ height: "80vh", overflow: "scroll" }}
+          >
+            {course.lessons.map((lesson, index) => (
+              <Item
+                onClick={() => setClicked(index)}
+                key={index}
+                icon={<Avatar>{index + 1}</Avatar>}
+              >
+                {lesson.title.substring(0, 30)}
+              </Item>
+            ))}
+          </Menu>
+        </div>
+
+        <div className="col">
+          {clicked !== -1 ? (
+            <>{JSON.stringify(course.lessons[clicked])}</>
+          ) : (
+            <>Click on the lesson to start learning</>
+          )}
+        </div>
+      </div>
     </StudentRoute>
   );
 };
