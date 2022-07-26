acde7789aefa25b7ee3ffec1ee36959e97780e26
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-26 14:37:49 +0530

    user course

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,12 +1,27 @@
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const SingleCourse = () => {
-    const router = useRouter()
+  const router = useRouter();
+  const { slug } = router.query;
 
-    return(
-        <>
-            <h1>{JSON.stringify(router.query.slug)}</h1>
-        </>
-    )
-}
-export default SingleCourse
+  const [loading, setLoading] = useState(false);
+  const [course, setCourse] = useState({ lessons: [] });
+
+  useEffect(() => {
+    if (slug) loadCourse();
+  }, [slug]);
+
+  const loadCourse = async () => {
+    const { data } = await axios.get(`/api/course/${slug}`);
+    setCourse(data);
+  };
+
+  return (
+    <>
+      <h1>{JSON.stringify(course, null, 4)}</h1>
+    </>
+  );
+};
+export default SingleCourse;
