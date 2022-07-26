e2932a2451d2d2fd5aff71561c58dd62dd41a8e6
deathmukh <ishanideshmukh2002@gmail.com>
2022-07-26 13:07:22 +0530

    success redirect

---
+++ client/pages/stripe/success/[id].js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { SyncOutlined } from "@ant-design/icons";
+import UserRoute from "../../../components/routes/UserRoute";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const StripeSuccess = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (id) successRequest();
+  }, [id]);
+
+  const successRequest = async () => {
+    const { data } = await axios.get(`/api/stripe-success/${id}`);
+    router.push(`/user/course/${data.slug}`);
+  };
+
+  return (
+    <UserRoute showNav={false}>
+      <div className="row text-center">
+        <div className="col-md-9 pb-5">
+          <div d-flex justify-content-center p-5>
+            <SyncOutlined spin className="display-1 text-danger p-5" />
+          </div>
+        </div>
+        <div className="col-md-3"></div>
+      </div>
+    </UserRoute>
+  );
+};
+
+export default StripeSuccess;
