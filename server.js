65581b04e78426992122a0feb92a2f7cf87939ad
alphanoobie <hussaingadi.hg@gmail.com>
2022-07-27 12:00:56 +0530

    idk

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
  }
 
