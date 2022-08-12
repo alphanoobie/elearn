eef9822c7363c2dd0abfadb3a353b0414bf4fbb3
alphanoobie <hussaingadi.hg@gmail.com>
2022-08-13 00:41:02 +0530

    changed imports

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
-const morgan = require("morgan");
-require("dotenv").config();
+import morgan from "morgan";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 import mongoose from "mongoose";
 import csrf from "csurf";
 import cookieParser from "cookie-parser";
@@ -28,6 +29,7 @@ app.use(cookieParser());
 
 //route
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
 
 //
 app.use(csrfProtection);
